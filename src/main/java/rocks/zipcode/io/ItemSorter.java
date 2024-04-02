af015965030e8cdbf2c0114960daf4e0cefc84c6
LavanyaPalanikumar93 <lavanyasaii93@gmail.com>
2024-04-02 18:49:30 -0400

    Done

--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -1,15 +1,25 @@
 package rocks.zipcode.io;
 
+import rocks.zipcode.io.comparators.IdComparator;
+
+import java.util.Arrays;
 import java.util.Comparator;
 
 /**
  * @author leon on 30/01/2019.
  */
 public class ItemSorter {
+    Item[] items;
     public ItemSorter(Item[] items) {
+        this.items=items;
+
     }
 
     public Item[] sort(Comparator<Item> comparator) {
-        return null;
+        Arrays.sort(items,comparator);
+        return items;
+
+
+        //return null;
     }
 }
