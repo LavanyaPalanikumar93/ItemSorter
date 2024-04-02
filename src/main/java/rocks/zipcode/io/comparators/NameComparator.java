d3385cd3bc59f865c681638aa503ab352fdbe33b
LavanyaPalanikumar93 <lavanyasaii93@gmail.com>
2024-04-02 16:23:46 -0400

    All tests are passing

--- src/main/java/rocks/zipcode/io/comparators/NameComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/NameComparator.java
@@ -1,7 +1,18 @@
 package rocks.zipcode.io.comparators;
+import rocks.zipcode.io.Item;
 
+import java.util.Comparator;
 /**
  * @author leon on 30/01/2019.
  */
-public class NameComparator {
+public class NameComparator implements Comparator<Item> {
+    @Override
+    public int compare(Item t1, Item t2){
+
+        return t1.getName().compareTo(t2.getName());
+
+
+    }
+
+
 }
--- src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
@@ -1,7 +1,15 @@
 package rocks.zipcode.io.comparators;
+import rocks.zipcode.io.Item;
 
+import java.util.Comparator;
 /**
  * @author leon on 30/01/2019.
  */
-public class PriceComparator {
+public class PriceComparator implements Comparator<Item> {
+    @Override
+    public int compare(Item item, Item t1) {
+        return item.getPrice().compareTo(t1.getPrice());
+    }
+
+
 }
